ce1f35d93419341c3d9961037cd74b48e67e6695
Atharva Kulkarni <atharvakulkarni2204@gmail.com>
2020-11-12 14:53:06 +0530

    added auth class

---
+++ lib/helpers/authenticate.dart
@@ -0,0 +1,31 @@
+import 'package:Messenger/views/signin.dart';
+import 'package:Messenger/views/signup.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget
+{
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate>
+{
+  bool showSignIn = true;
+
+  void toggleView()
+  {
+    setState(()
+    {
+      showSignIn = !showSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context)
+  {
+    if(showSignIn)
+      return SignIn(toogleView: toggleView);
+    else
+      return SignUp(toogleView: toggleView);
+  }
+}
